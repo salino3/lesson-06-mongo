a61427df187e161c4137b54a6929f1d10e16b43c
salino3 <programacionflavio@gmail.com>
2024-02-02 14:00:43 +0100

    create some objects to data base

---
+++ src/core/servers/db.server.ts
@@ -0,0 +1,11 @@
+import {Db, MongoClient} from 'mongodb';
+
+export let db: Db;
+
+export const connectToDBServer = async (connectionURI: string) => {
+  const client = new MongoClient(connectionURI);
+  await client.connect();
+
+  db = client.db();
+}
+
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {
   logRequestMiddleware,
   logErrorRequestMiddleware,
 } from "#common/middlewares/index.js";
-import { createRestApiServer } from "#core/servers/index.js";
+import { createRestApiServer, connectToDBServer, db } from "#core/servers/index.js";
 import { envConstants } from "#core/constants/index.js";
 import { booksApi } from "#pods/book/index.js";
 
@@ -22,6 +22,15 @@ restApiServer.use("/api/books", booksApi);
 
 restApiServer.use(logErrorRequestMiddleware);
 
-restApiServer.listen(envConstants.PORT, () => {
+restApiServer.listen(envConstants.PORT, async () => {
+  if(envConstants.isApiMock){
+    console.log("Running API mock");
+  }else{
+    await connectToDBServer(envConstants.MONGODB_URI);
+    // await db.collection('books').insertOne({ name: 'Book 1' });
+    const books = await db.collection("books").find().toArray();
+    console.log({books})
+  }
+
   console.log(`Server ready at port ${envConstants.PORT}`);
 });
