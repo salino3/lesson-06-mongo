d7d41c8ccf9ae4ef1c7554cbe0adc12d5cc4f4fb
salino3 <programacionflavio@gmail.com>
2024-02-03 11:44:11 +0100

    commit

--- src/dals/book/repositories/book.db-repository.ts
+++ src/dals/book/repositories/book.db-repository.ts
@@ -1,22 +1,49 @@
-import {db} from '#core/servers/index.js';
+import { ObjectId } from "mongodb";
 import { BookRepository } from "./book.repository.js";
+import {getBookContext} from '../book.context.js';
 import { Book } from "../book.model.js";
 
 export const dbRepository: BookRepository = {
   getBookList: async (page?: number, pageSize?: number) => {
-    return await db.collection<Book>("books").find().toArray();
+    const skip = Boolean(page) ? (page - 1) * pageSize : 0;
+    const limit = pageSize ?? 0;
+    return await getBookContext()
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .toArray();
   },
   getBook: async (id: string) => {
-    throw new Error("Not implemented");
+    return await getBookContext()
+    .findOne({
+      _id: new ObjectId(id)
+    });
   },
   saveBook: async (book: Book) => {
-   const {insertedId} = await db.collection<Book>("books").insertOne(book);
-   return {
-    ...book,
-    _id: insertedId
-   };
+    // upsert
+    const result = await getBookContext()
+      .findOneAndUpdate(
+        {
+          _id: book._id,
+        },
+        {
+          $set: book,
+        },
+        {
+          upsert: true,
+          returnDocument: 'after',
+        }
+      );
+ const updatedBook = result || book;
+
+ return updatedBook;
   },
+  //
+
   deleteBook: async (id: string) => {
-    throw new Error("Not implemented");
+   const {deletedCount} =  await getBookContext().deleteOne({
+      _id: new ObjectId(id)
+    });
+    return deletedCount === 1;
   },
 };
